b051ef44574584b1af15b0feec4862bcf425454b
Shane <navysupra@gmail.com>
2022-08-05 05:02:41 +0000

    starting to flesh out fetchCoordsById

--- iss.js
+++ iss.js
@@ -6,16 +6,20 @@ const fetchIp = `https://api64.ipify.org/?format=json`;
 
 /* Arguments */
 /* Export Functions */
+// Fetchs my ip address
 const fetchMyIp = (callback) => {
   request(fetchIp, (error, response, body) => {
     if (error) return callback(error, null);
     if (response.statusCode !== 200) {
-      callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null)
+      callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
       return;
     }
     const ip = JSON.parse(body).ip;
     callback(null, ip);
   });
+};
+const fetchCoordsByIp = (ip, callback) => {
+
 };
 
 /* Local Functions */
