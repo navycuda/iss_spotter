e3bead2011bd79d66adf879cd95dd026fc6bad80
Shane <navysupra@gmail.com>
2022-08-05 04:58:54 +0000

    seem to have the basic function of collecting the ip in place

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 /* Require */
 const request = require(`request`);
 const { outputToConsole } = require(`@navycuda/lotide`);
+const { fetchMyIp } = require("./iss");
 
 /* Tcp:Http */
 /* Arguments */
 /* Export Functions */
 /* Local Functions */
-
-const fetchMyIp = () => {
-
-};
-
 /* Execution & Test Data */
+fetchMyIp((error, ip) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+  console.log('It worked! Returned IP:' , ip);
+});
+
 /* Exports */
--- iss.js
+++ iss.js
@@ -1,7 +1,24 @@
 /* Require */
+const request = require(`request`);
+
 /* Tcp:Http */
+const fetchIp = `https://api64.ipify.org/?format=json`;
+
 /* Arguments */
 /* Export Functions */
+const fetchMyIp = (callback) => {
+  request(fetchIp, (error, response, body) => {
+    if (error) return callback(error, null);
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null)
+      return;
+    }
+    const ip = JSON.parse(body).ip;
+    callback(null, ip);
+  });
+};
+
 /* Local Functions */
 /* Execution & Test Data */
 /* Exports */
+module.exports = { fetchMyIp };
