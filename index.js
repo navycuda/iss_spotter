4100540955d45d1a72ba85f5bd51387570a816da
Shane <navysupra@gmail.com>
2022-08-05 02:31:12 +0000

    fleshing out the index

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 /* Require */
+const request = require(`request`);
+
 /* Tcp:Http */
 /* Arguments */
 /* Export Functions */
 /* Local Functions */
+const outputToConsole = (error, result) => {
+
+};
+const fetchMyIp = () => {
+
+};
+
 /* Execution & Test Data */
 /* Exports */
