6753e0cfa48513354a5e7c0a8918676a88c44fca
Shane <navysupra@gmail.com>
2022-08-05 02:38:04 +0000

    required @navycuda/lotide

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 /* Require */
 const request = require(`request`);
+const { outputToConsole } = require(`@navycuda/lotide`);
 
 /* Tcp:Http */
 /* Arguments */
 /* Export Functions */
 /* Local Functions */
-const outputToConsole = (error, result) => {
 
-};
 const fetchMyIp = () => {
 
 };
