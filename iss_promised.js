098bcc8f3321d6e076a8bbcb3fa71fb9d5dd0d44
Shane <navysupra@gmail.com>
2022-08-07 07:06:02 +0000

    laying out functions

--- iss_promised.js
+++ iss_promised.js
@@ -1,7 +1,15 @@
 /* Require */
+const request = require(`request-promise-native`);
+
 /* Tcp:Http */
+const urlIp = `https://api64.ipify.org/?format=json`;
+
 /* Arguments */
 /* Export Functions */
+const fetchMyIp = () => {
+
+};
+
 /* Local Functions */
 /* Execution & Test Data */
 /* Exports */
