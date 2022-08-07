0f6c518e682d2f9007fdd702c1f313748d9a9de8
Shane <navysupra@gmail.com>
2022-08-07 19:50:43 +0000

    added simple error handling

--- index2.js
+++ index2.js
@@ -1,14 +1,24 @@
 /* Require */
-const { fetchMyIp, fetchCoordsByIP, fetchISSFlyOverTimes  } = require(`./iss_promised`);
+const { nextIssTimesForMyLocation  } = require(`./iss_promised`);
 
 /* Tcp:Http */
 /* Arguments */
 /* Export Functions */
 /* Local Functions */
+const outputToConsole = (obj) => {
+  console.log(obj);
+};
 /* Execution & Test Data */
-fetchMyIp()
-  .then(fetchCoordsByIP)
-  .then(fetchISSFlyOverTimes)
-  .then(body => console.log(body));
+// fetchMyIp()
+//   .then(fetchCoordsByIP)
+//   .then(fetchISSFlyOverTimes)
+//   .then(body => console.log(body));
+nextIssTimesForMyLocation()
+  .then((flyoverTimes) => {
+    outputToConsole(flyoverTimes);
+  })
+  .catch((error) => {
+    console.log(`Error: `, error.message);
+  });
 
 /* Exports */
