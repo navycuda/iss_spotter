4594ce5410dc58dfff5681a9e4317c54d16040fa
Shane <navysupra@gmail.com>
2022-08-07 19:06:58 +0000

    Adding get my coordinates

--- iss_promised.js
+++ iss_promised.js
@@ -3,14 +3,19 @@ const request = require(`request-promise-native`);
 
 /* Tcp:Http */
 const urlIp = `https://api64.ipify.org/?format=json`;
+const urlGps = `http://ipwho.is/`;
+
 
 /* Arguments */
 /* Export Functions */
 const fetchMyIp = () => {
   return request(urlIp);
 };
+const fetchCoordsByIP = (body) => {
+  return request(urlGps + body.ip);
+};
 
 /* Local Functions */
 /* Execution & Test Data */
 /* Exports */
-module.exports = { fetchMyIp  };
+module.exports = { fetchMyIp, fetchCoordsByIP };
